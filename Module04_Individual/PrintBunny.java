0983d7660c42d6f7333738d1c3897108a1d35d53
MariaBuico <mariabuicosrbidev@gmail.com>
2025-11-15 13:53:04 -0500

    Added PrintBunny.java

---
+++ Module04_Individual/PrintBunny.java
@@ -0,0 +1,9 @@
+public class PrintBunny {
+   public static void main(String[] args) {
+      // write your code here below
+      System.out.println("(\\(\\");
+      System.out.println("(-.-)");
+      System.out.println("O_(\")(\")");
+      
+   }
+}
