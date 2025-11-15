ce7b3d89f043f8e79821a7caa0df0b783f091e28
MariaBuico <mariabuicosrbidev@gmail.com>
2025-11-15 13:46:00 -0500

    Create PrintKitty.java

---
+++ Module04_Individual/PrintKitty.java
@@ -0,0 +1,9 @@
+public class PrintKitty {
+   public static void main(String[] args) {
+      // write your code here below
+      System.out.println(" /\\_/\\");
+      System.out.println("( o.o )");
+      System.out.println(" > ^ <");
+      
+   }
+}
