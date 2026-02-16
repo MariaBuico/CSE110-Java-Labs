3e3f7b3d80132b828906f63131520d235faa0164
MariaBuico <mariabuicosrbidev@gmail.com>
2026-02-15 21:49:01 -0500

    Add PrintSpider.java

---
+++ Module06_Individual/PrintSpider.java
@@ -0,0 +1,10 @@
+public class PrintSpider {
+   public static void main(String[] args) {
+      // write your code here below
+      
+     System.out.println("  / _ \\");
+     System.out.println("\\_\\(_)/_/");
+     System.out.println(" _//\"\\\\_"); 
+     System.out.println("  /   \\"); 
+   }
+}
